70ebfe782a4a02b2ee4ada2015913f46274a0883
Jérémy FRERE <frere.jeremy@gmail.com>
2023-11-03 10:50:57 +0100

    First working version with already 54 rules.

---
+++ README.md
@@ -0,0 +1,7 @@
+# tibetan-normalizer
+
+A simple tool to normalize Tibetan Unicode strings.
+
+## TODO
+
+* Add a complete normalization of combined letters (like  for སྤྲ).
---
+++ src/tibetan-normalizer.js
@@ -0,0 +1,74 @@
+export default {
+
+  normalize (text) {
+    var normalized = this.normalizeCombinedLetters(text);
+    normalized = this.normalizeTsheks(normalized);
+    return normalized;
+  },
+
+  normalizeTsheks (text) {
+    return text
+      .replace(/(ཾ)([ཱཱཱེིོིྀུུ])/g, '$2$1') // Malformed: anusvara before vowel
+      .replace(/༌/g, '་') // Alternative tshek
+      .replace(/་+/g, '་'); // Multiple consecutive tsheks into one
+  },
+
+  normalizeCombinedLetters (text) {
+    return text
+      .replace(/[ ]/g, ' ')
+      .replace(/ༀ/g, 'ཨོཾ')
+      .replace(/ཀྵ/g, 'ཀྵ')
+      .replace(/བྷ/g, 'བྷ')
+      .replace(/ི+/g, 'ི')
+      .replace(/ུ+/g, 'ུ')
+      .replace(/ཱུ/g, 'ཱུ')
+      .replace(/ཱི/g, 'ཱི')
+      .replace(/ཱྀ/g, 'ཱྀ')
+      .replace(/དྷ/g, 'དྷ')
+      .replace(/གྷ/g, 'གྷ')
+      .replace(/ཪླ/g, 'རླ')
+      .replace(/ྡྷ/g, 'ྡྷ')
+      .replace(//g, '࿓༅')
+      .replace(//g, 'སྤྲ')
+      .replace(//g, 'ུ')
+      .replace(//g, 'ག')
+      .replace(//g, 'ུ')
+      .replace(//g, 'རྒྱ')
+      .replace(//g, 'གྲ')
+      .replace(//g, 'ུ')
+      .replace(//g, 'ི')
+      .replace(//g, 'བྱ')
+      .replace(//g, 'སྲ')
+      .replace(//g, 'སྒྲ')
+      .replace(//g, 'ལྷ')
+      .replace(//g, 'ོ')
+      .replace(//g, 'གྱ')
+      .replace(//g, 'རླ')
+      .replace(//g, 'ཕྱ')
+      .replace(//g, 'སྩ')
+      .replace(//g, 'རྡ')
+      .replace(//g, 'རྗ')
+      .replace(//g, 'དྲྭ')
+      .replace(//g, 'ཛྲ')
+      .replace(//g, 'ལྷྭ')
+      .replace(//g, 'སྤྱ')
+      .replace(//g, 'བྷྱ')
+      .replace(//g, 'གྷྣ')
+      .replace(//g, 'གྷྲ')
+      .replace(//g, 'ནྡྷ')
+      .replace(//g, 'ཧྣ')
+      .replace(//g, 'ཥྚ')
+      .replace(//g, 'བྷྲ')
+      .replace(//g, 'ཱུ')
+      .replace(//g, 'ཱུ')
+      .replace(//g, 'རྒྷ')
+      .replace(//g, 'ྀ')
+      .replace(//g, 'ཱ')
+      .replace(//g, 'ཱ')
+      .replace(//g, 'དྡྷྭ')
+      .replace(//g, 'ིཾ')
+      .replace(//g, 'ིཾ')
+      .replace(//g, 'ངྷ')
+  }
+
+}
